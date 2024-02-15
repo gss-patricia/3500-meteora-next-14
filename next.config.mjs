633239634fbe961a6d97c2989957fdd2f67c25fc
gss-patricia <gss.patricia@gmail.com>
2024-02-15 16:19:18 +0000

    video 4.2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  trailingSlash: true,
   images: {
-    loader: "custom",
-    loaderFile: "./src/app/imagem.js",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
